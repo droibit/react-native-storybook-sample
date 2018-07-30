4c4604c88e2dbddac7466f40fd8a81eac9ba93f0
Shinya Kumagai <roomful.rooms@gmail.com>
2018-07-30 18:13:42 +0900

    Add react-native-storybook-loader

--- storybook/stories/index.tsx
+++ storybook/stories/index.tsx
@@ -6,21 +6,8 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 // tslint:disable-next-line
-import Button from './Button';
-import CenterView from './CenterView';
-import Welcome from './Welcome';
+import CenterView from '../../src/CenterView';
+import Welcome from '../../src/Welcome';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
-storiesOf('Button', module)
-  .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
-  .add('with text', () => (
-    <Button onPress={action('clicked-text')}>
-      <Text>Hello Button</Text>
-    </Button>
-  ))
-  .add('with some emoji', () => (
-    <Button onPress={action('clicked-emoji')}>
-      <Text>😀 😎 👍 💯</Text>
-    </Button>
-  ));
--- storybook/storybook.js
+++ storybook/storybook.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
+import { loadStories } from "./storyLoader";
 // tslint:disable-next-line
 import { getStorybookUI, configure } from '@storybook/react-native';
 
 // import stories
 configure(() => {
-  require('./stories');
+  loadStories()
 }, module);
 
 // This assumes that storybook is running on the same host as your RN packager,
